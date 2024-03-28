f7175d58bb60929e777cff5825cbdc8059a199c3
Faiaz <48489726+Faiaz3k@users.noreply.github.com>
2024-03-28 19:53:50 +0600

    Update Booster.py
    need to test v1

--- Windows/Booster.py
+++ Windows/Booster.py
@@ -1,3 +1,4 @@
+import sys
 from ctypes import cast, POINTER
 from comtypes import CLSCTX_ALL
 from pycaw.pycaw import AudioUtilities, IAudioEndpointVolume
@@ -8,7 +9,12 @@ def increase_volume(increase_percent):
         IAudioEndpointVolume._iid_, CLSCTX_ALL, None)
     volume = cast(interface, POINTER(IAudioEndpointVolume))
     current_volume = volume.GetMasterVolumeLevelScalar()
-    target_volume = min(1.0, current_volume * (1.0 + increase_percent / 100))  # Increase by the specified percentage
+    target_volume = min(1.0, current_volume + increase_percent / 100)  # Increase by specified percentage
     volume.SetMasterVolumeLevelScalar(target_volume, None)
 
-increase_volume(20)  # Increase volume by 20%
+if __name__ == "__main__":
+    if len(sys.argv) > 1:
+        percentage = float(sys.argv[1])
+        increase_volume(percentage)
+    else:
+        print("Usage: python booster.py <increase_percent>")
