ed902e4fc0562715dc09339babd28b78f092c2ad
Faiaz <48489726+Faiaz3k@users.noreply.github.com>
2024-03-28 19:55:33 +0600

    Update Booster.py
    booster v2 need to test

--- Linux/Booster.py
+++ Linux/Booster.py
@@ -1,18 +1,16 @@
+import sys
 import pulsectl
 
 def increase_volume(increase_percent):
     with pulsectl.Pulse('increase-volume') as pulse:
-        # Get the default sink (output device)
         sink = pulse.sink_list()[0]
-        
-        # Get the current volume
         current_volume = sink.volume.value_flat
-        
-        # Calculate the target volume after increasing by the specified percentage
-        target_volume = min(1.0, current_volume + increase_percent / 100)
-        
-        # Set the new volume
+        target_volume = min(1.0, current_volume + increase_percent / 100)  # Increase by specified percentage
         pulse.volume_set_all_chans(sink, target_volume)
 
-# Example: Increase volume by 20%
-increase_volume(20)
+if __name__ == "__main__":
+    if len(sys.argv) > 1:
+        percentage = float(sys.argv[1])
+        increase_volume(percentage)
+    else:
+        print("Usage: python booster.py <increase_percent>")
