2af2204ca89a475d6a07281f91da135dab6ed3dd
Faiaz <48489726+Faiaz3k@users.noreply.github.com>
2024-03-28 19:54:41 +0600

    Update Booster.py
    need to test v1

--- MacOs/Booster.py
+++ MacOs/Booster.py
@@ -1,3 +1,4 @@
+import sys
 import AppKit
 from Foundation import NSBundle
 
@@ -6,8 +7,12 @@ def increase_volume(increase_percent):
     system_sound = NSSound.alloc().initWithContentsOfFile_byReference_('/System/Library/LoginPlugins/BezelServices.loginPlugin/Contents/Resources/volume.aiff', True)
     system_sound.play()
     current_volume = AppKit.NSSound.systemVolume()
-    target_volume = min(1.0, current_volume + increase_percent / 100)
+    target_volume = min(1.0, current_volume + increase_percent / 100)  # Increase by specified percentage
     AppKit.NSSound.setSystemVolume_(target_volume)
 
-# Example: Increase volume by 20%
-increase_volume(20)
+if __name__ == "__main__":
+    if len(sys.argv) > 1:
+        percentage = float(sys.argv[1])
+        increase_volume(percentage)
+    else:
+        print("Usage: python booster.py <increase_percent>")
